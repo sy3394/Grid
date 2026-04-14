9fbf9970ade4dd7bc8c179b32df5506a163401cd
Shuhei Yamamoto <shuhei.yamamoto.2011@gmail.com>
2026-04-13 20:08:11 -0400

    sourceme: add HDF5 lib path to LD_LIBRARY_PATH at runtime
    Compute_DWF_G5R5 (and other binaries built --with-hdf5) link against
    libhdf5.so.310 but sourceme.sh did not expose $HDF5/lib at runtime,
    causing "cannot open shared object file: No such file or directory".

--- systems/Frontier/sourceme.sh
+++ systems/Frontier/sourceme.sh
@@ -11,6 +11,10 @@ module load craype-accel-amd-gfx90a
 export LD_LIBRARY_PATH=/opt/cray/libfabric/1.20.1/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/gcc/mpfr/3.1.4/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/:$LD_LIBRARY_PATH
+
+# HDF5 runtime library (needed by Compute_DWF_G5R5 and other binaries linked --with-hdf5)
+export HDF5=$(spack location -i hdf5%gcc@14.2.0)
+export LD_LIBRARY_PATH=$HDF5/lib:$LD_LIBRARY_PATH
 ln -s /opt/rocm-6.0.0/lib/libamdhip64.so.6 .
 
 #echo spack load c-lime
